5fed82c799604fcbd4f31def994558230238c333
tushar <tush44644@gmail.com>
2024-02-12 13:02:07 +0530

    created complete chat page

--- src/components/chat/chatbox/Index.tsx
+++ src/components/chat/chatbox/Index.tsx
@@ -1,5 +1,6 @@
 import {Fragment,memo} from 'react';
 import Userchat from "./chat/Index";
+import Button from "../../button/Btn";
 
 function Index() {
   return (
@@ -37,7 +38,29 @@ function Index() {
                         <div className="col-12 px-2 py-2">
                            <Userchat />
                         </div>
-            <div className="col-12"></div>
+            <div className="col-12">
+               <div className="row mx-0">
+                  <div className="col-12 d-flex justify-content-center align-items-center">
+                     <div className='d-flex px-2 align-items-center' style={{backgroundColor:"#FFF",border:"solid #FFFF",borderRadius:"8px",boxShadow:"0px 0xp 2px"}}>
+                        <span className="pe-1"><input type="text" className='form-control' style={{border:"none"}} placeholder='Type your message here...' /></span>
+                        <span className='mx-3'>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-mic-fill" viewBox="0 0 16 16">
+                              <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z"/>
+                              <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
+                           </svg>
+                        </span>
+                        <span className='mx-2'>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-paperclip" viewBox="0 0 16 16">
+                              <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z"/>
+                           </svg>
+                        </span>
+                        <span className='mx-2 chatBtn'>
+                           <Button width='' paddingX='' paddingY='' clickfunc={() => {}} content='send' />
+                        </span>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
     </Fragment>
   )
